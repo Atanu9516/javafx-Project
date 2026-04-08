6589110a1d39d806273e5bede0e8dba9574ef33f
bhowm <bhowmicatanu71@gmail.com>
2026-04-08 13:04:54 +0600

    Fix module-info.java with proper module declaration

--- ONLINEMCQEXAM/src/main/java/module-info.java
+++ ONLINEMCQEXAM/src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module com.example.onlinemcqexam {
+module com.example.onlinemcqexam {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.fontawesome5;
-
-    opens com.example.onlinemcqexam to javafx.fxml;
+    
     exports com.example.onlinemcqexam;
+    opens com.example.onlinemcqexam to javafx.fxml;
 }
